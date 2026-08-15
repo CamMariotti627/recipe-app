26001329e757113d0b6fbdefd9b7260a166db137
Cam <cam.mariotti@pointpark.edu>
2026-08-15 19:54:11 -0400

    updated GET /recipes to /api/vi/recipes to query real postgres data

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const pool = require('./db');
 const express = require("express");
 const app = express();
-const port = 4000;
-app.get("/recipes", (req,res) => {
-    res.send("Here are some recipes!");
-});
+const port = process.env.PORT || 4000;
+//JS route handler = now async waits on database query
+app.get("/api/v1/recipes", async (req, res) => {
+    try {
+        //SQL -selects every column/row from recipes table
+        const result = await pool.query('SELECT * FROM recipes');
+        //JS - sends rows back to JSON
+        res.json(result.rows);
+    } catch (err) {
+    //JS-if query fails for any reason, doesn't crash server
+    // sends back err response
+    console.error(err);
+    res.status(500).json( { error: 'Something went wrong fetching your recipes.'});
+    }
+})
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
 });
