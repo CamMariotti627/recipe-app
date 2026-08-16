b17459c9fc3bd89cadda2db7641665e4adfa3e23
Cam <cam.mariotti@pointpark.edu>
2026-08-15 21:45:46 -0400

    fixed db.js pools to correct connection errors, draw from dburl & local host if unavailable

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({
+//- if DATABASE_URL exists (render) use it directly
+// otherwise, fallback to local connection
+
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {rejectUnauthorized: false}
+    })
+    : new Pool ({
     user: 'postgres',
     host: 'localhost',
     database: 'recipe_app_db',
